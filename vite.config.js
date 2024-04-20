7c9c5f589afa4792c938f33fef80fdf59904461b
Prashant <prashantyadav09783@gmail.com>
2024-04-20 15:16:22 +0530

    production ready

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target : "http://localhost:8080",
+        target : "nexum-backend-production.up.railway.app",
         secure: false,
       }
     }
