be829416e7d13b334b680fa7dfc4c795698a82aa
0PrashantYadav0 <prashantyadav09783@gmail.com>
2024-06-16 23:14:53 +0530

    changes in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   server: {
     proxy:{
       '/api':{
         target : "https://nexum-backend-production-486e.up.railway.app/",
         secure: false,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   }
